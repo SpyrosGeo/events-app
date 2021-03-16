7a64bd2378e65428e4941ffc710b2d6e93044e5f
SpyrosGeo <xarodasimba@gmail.com>
2021-03-16 11:22:07 +0200

    extracting data on the Catch-all Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,23 @@
-
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../dummy-data';
 export default function FilteredEventsPage() {
+    const router = useRouter()
+    const filteredData = router.query.slug;
+    if (!filteredData) {
+        return <p className="center">Loading...</p>
+    }
+    const filteredYear = filteredData[0]
+    const filteredMonth = filteredData[1]
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+    //check for valid parameteres
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return <p>Invalid filter. Please adjust your values</p>
+    }
+    const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
+    if(!filteredEvents || filteredEvents.length===0) {
+        return <p className="center">No Events found</p>
+    }
     return (
         <div>
             <h1>Filetered Events</h1>
