db458a6238506732e3daeaa9aaa3fe2d32961d0e
SpyrosGeo <xarodasimba@gmail.com>
2021-09-20 22:56:39 +0300

    added static head content

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-// import Head from 'next/head'
+import Head from 'next/head'
 import EventList from "../components/events/event-list"
-// import {  getFeaturedEvents,  } from "../dummy-data"
 import {getFeaturedEvents } from "../helpers/api-util"
 export default function Home({featuredEvents}) {
-  // console.log(featuredEvents)
   return (
     <div>
+      <Head>
+        <title>Nextjs Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve" />
+      </Head>
      <EventList items={featuredEvents} />
     </div>
 
